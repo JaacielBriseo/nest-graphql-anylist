52bb73d7b83b0596e86b5083205b6974e0199198
JaacielBriseo <jaacielbris23@gmail.com>
2023-05-20 01:03:10 -0700

    Auth by roles

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -1,21 +1,31 @@
 import {
   ExecutionContext,
+  ForbiddenException,
   InternalServerErrorException,
   createParamDecorator,
 } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { ValidRoles } from '../enums/valid-roles.enum';
+import { User } from '../../users/entities/user.entity';
 
 export const CurrentUser = createParamDecorator(
-  (roles = [], context: ExecutionContext) => {
+  (roles: ValidRoles[] = [], context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context);
-    const user = ctx.getContext().req.user;
-
+    const user: User = ctx.getContext().req.user;
     if (!user) {
       throw new InternalServerErrorException(
         `No user on request - make sure that we use the AuthGuard`,
       );
     }
+    if (roles.length === 0) return user;
 
-    return user;
+    for (const role of user.roles) {
+      if (roles.includes(role as ValidRoles)) {
+        return user;
+      }
+    }
+    throw new ForbiddenException(
+      `User ${user.fullName} need some of this roles : ${roles}`,
+    );
   },
 );
