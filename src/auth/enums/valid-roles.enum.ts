a3191d27ab973fa990c07a90e620d9b8bc62b72b
JaacielBriseo <jaacielbris23@gmail.com>
2023-06-01 20:14:47 -0700

    Custom Args [ValidRoles] - GraphQL Enums

--- src/auth/enums/valid-roles.enum.ts
+++ src/auth/enums/valid-roles.enum.ts
@@ -1,5 +1,11 @@
+import { registerEnumType } from '@nestjs/graphql';
+
 export enum ValidRoles {
   admin = 'admin',
   user = 'user',
   superUser = 'superUser',
 }
+registerEnumType(ValidRoles, {
+  name: 'ValidRoles',
+  description: 'Roles permitidos para la enumeracion de roles',
+});
