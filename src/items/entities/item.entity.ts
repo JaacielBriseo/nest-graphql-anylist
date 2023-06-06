0ed6f714c17fc2b886079d7b82de72a8376e097e
JaacielBriseo <jaacielbris23@gmail.com>
2023-06-05 17:56:49 -0700

    ListItems relations

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -4,9 +4,11 @@ import {
   Entity,
   Index,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
+import { ListItem } from '../../list-item/entities/list-item.entity';
 
 @Entity({ name: 'items' })
 @ObjectType()
@@ -27,10 +29,12 @@ export class Item {
   @Field(() => String, { nullable: true })
   quantityUnits?: string;
 
-  //stores
-  //user
   @ManyToOne(() => User, (user) => user.items, { nullable: false, lazy: true })
   @Index('userId-index')
   @Field(() => User)
   user: User;
+
+  @OneToMany(() => ListItem, (listItem) => listItem.item, { lazy: true })
+  @Field(() => [ListItem])
+  listItem: ListItem[];
 }
